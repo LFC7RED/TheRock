74a0c94421e880b95829be192cadfb6509b76ffd
LFC7RED <32332883+LFC7RED@users.noreply.github.com>
2017-10-31 15:32:48 +0000

    javascript added
    added javascript and made changes to the store.

---
+++ myScript.js
@@ -0,0 +1,9 @@
+ $(acting.html).ready(function(){
+      $('.parallax').parallax();}); 
+
+
+
+ // Initialize collapse button
+  $(".button-collapse").sideNav();
+  // Initialize collapsible (uncomment the line below if you use the dropdown variation)
+  //$('.collapsible').collapsible();
