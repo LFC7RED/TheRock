866590f6fc3444acc8d2d9589c73d1ae9e7a3faa
LFC7RED <32332883+LFC7RED@users.noreply.github.com>
2017-11-02 18:34:57 +0000

    14

--- myScript.js
+++ myScript.js
@@ -5,3 +5,11 @@
   $(".button-collapse").sideNav();
   // Initialize collapsible (uncomment the line below if you use the dropdown variation)
   //$('.collapsible').collapsible();
+
+$(".bg").interactive_bg({
+   strength: 25,
+   scale: 1.05,
+   animationSpeed: "100ms",
+   contain: true,
+   wrapContent: false
+ });
