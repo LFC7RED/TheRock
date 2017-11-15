00c3f62779f387029db470078f0796ffc89c1c57
LFC7RED <32332883+LFC7RED@users.noreply.github.com>
2017-11-15 14:55:30 +0000

    23

--- myScript.js
+++ myScript.js
@@ -11,6 +11,8 @@ $(document).ready(function(){
 $(document).ready(function(){
  $(".button-collapse").sideNav();
     });
+
+
  // Initialize collapse button
   $(".button-collapse").sideNav();
   // Initialize collapsible (uncomment the line below if you use the dropdown variation)
